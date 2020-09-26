9a70f7e69318a426537f6c2ad2701be7ed55fce9
Nikosar <randomregistr@gmail.com>
2020-09-26 11:33:07 +0300

    Initial commit

---
+++ src/main/java/com/nikosar/animeforever/AnimeForeverApplication.java
@@ -0,0 +1,13 @@
+package com.nikosar.animeforever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeForeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeForeverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikosar/animeforever/AnimeForeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikosar.animeforever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeForeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
