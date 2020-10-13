4ab6168fa6c6aaa4e38dd1d882ef807617a2cf58
Nikosar <randomregistr@gmail.com>
2020-10-13 10:17:07 +0300

    Tests fixed

--- src/test/java/com/nikosar/animeforever/AnimeForeverApplicationTest.kt
+++ src/test/java/com/nikosar/animeforever/AnimeForeverApplicationTest.kt
@@ -1,9 +1,17 @@
 package com.nikosar.animeforever
 
 import com.nikosar.animeforever.discord.JDABot
+import io.mockk.every
+import io.mockk.mockk
+import net.dv8tion.jda.api.MessageBuilder
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.MessageChannel
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.requests.restaction.MessageAction
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import java.util.concurrent.CompletableFuture
 
 @SpringBootTest
 internal open class AnimeForeverApplicationTests {
@@ -13,4 +21,20 @@ internal open class AnimeForeverApplicationTests {
     @Test
     fun contextLoads() {
     }
+}
+
+fun mockEvent(): MessageReceivedEvent {
+    val event = mockk<MessageReceivedEvent>()
+    val channel = mockk<MessageChannel>()
+    val messageAction = mockk<MessageAction>()
+    every { event.channel } returns channel
+    every { channel.sendMessage(any<String>()) } answers { call ->
+        val future = CompletableFuture<Message>()
+        val message = MessageBuilder(call.invocation.args[0] as String).build()
+        future.complete(message)
+
+        every { messageAction.submit() } returns future
+        messageAction
+    }
+    return event
 }
--- src/test/java/com/nikosar/animeforever/discord/command/HelpCommandTest.kt
+++ src/test/java/com/nikosar/animeforever/discord/command/HelpCommandTest.kt
@@ -1,6 +1,10 @@
 package com.nikosar.animeforever.discord.command
 
 import com.nikosar.animeforever.AnimeForeverApplicationTests
+import com.nikosar.animeforever.mockEvent
+import net.dv8tion.jda.api.entities.Message
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -9,9 +13,10 @@ internal class HelpCommandTest : AnimeForeverApplicationTests() {
     lateinit var helpCommand: HelpCommand
 
     @Test
-//    TODO think about how to test
     fun helpMePlease() {
-//        val helpMePlease = helpCommand.helpMePlease("", Mockito.mock(MessageReceivedEvent::class.java)).block()
-//        assertThat(helpMePlease, containsString("[!mockf, !mockfind] $BRAVE_TEST_PHRASE"))
+        val event = mockEvent()
+        val block: Message? = helpCommand.helpMePlease("", event).block()
+        val contentRaw = block?.contentRaw!!
+        assertThat(contentRaw, containsString("[!mockf, !mockfind] -> $BRAVE_TEST_PHRASE"))
     }
 }
